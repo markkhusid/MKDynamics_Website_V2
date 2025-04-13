d874ea89fe4856152e277e910f7142ab65ff4b8e
Mark Khusid <markkhusid@protonmail.com>
2025-04-13 19:10:36 -0400

    Added to CPP section and removed !dir output cells

---
+++ Sections/Computer_Programming/CPP/Learn_Modern_CPP/Cpp_Code/02_Variables_Scopes_and_Namespaces/02-swap.cpp
@@ -0,0 +1,13 @@
+// 02-swap.cpp : attempt to swap the values of an int and a double
+ 
+#include <print>
+using namespace std;
+ 
+int main() {
+    int a = 1;
+    double b = 2.5;
+    println("(1) a = {}, b = {}", a, b);
+    a = 2.5;
+    b = 1;
+    println("(2) a = {}, b = {}", a, b);
+}
