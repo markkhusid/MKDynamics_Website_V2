656c5005e77d204cece92673c65b1b59dba24707
Mark Khusid <markkhusid@protonmail.com>
2025-04-18 20:34:07 -0400

    Added to CPP programming section

---
+++ Sections/Computer_Programming/CPP/Learn_Modern_CPP/Cpp_Code/02_Variables_Scopes_and_Namespaces/02-scopes.cpp
@@ -0,0 +1,16 @@
+// 02-scopes.cpp : define three variables with the same name in one program
+ 
+#include <print>
+using namespace std;
+ 
+auto a{ 1.5f };
+ 
+int main() {
+    println("(1) {}", a);
+    auto a{ 2u };
+    println("(2) {}", a);
+    {
+        auto a{ 2.5 };
+        println("(3) {}", a);
+    }
+}
