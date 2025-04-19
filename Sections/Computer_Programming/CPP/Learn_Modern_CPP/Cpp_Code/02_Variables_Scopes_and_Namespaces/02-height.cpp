02ef1617371d71f7be5d9b5174223a937a6f4661
Mark Khusid <markkhusid@protonmail.com>
2025-04-19 11:58:51 -0400

    Added to CPP Programming section

---
+++ Sections/Computer_Programming/CPP/Learn_Modern_CPP/Cpp_Code/02_Variables_Scopes_and_Namespaces/02-height.cpp
@@ -0,0 +1,18 @@
+// 02-height.cpp : define the same variable name in two different namespaces
+ 
+#include <print>
+using namespace std;
+ 
+namespace Wonderland {
+    auto alice_height_m{ 0.15 };
+}
+ 
+namespace VictorianEngland {
+    auto alice_height_m{ 0.9 };
+}
+ 
+int main() {
+    println("Alice\'s height varies between {}m and {}m",
+        Wonderland::alice_height_m,
+        VictorianEngland::alice_height_m);
+}
