5afcc939dfe36964660ef1f4623bfa6c8169ca14
Mark Khusid <markkhusid@protonmail.com>
2025-04-23 06:11:25 -0400

    Added section constants to CPP programming

---
+++ Sections/Computer_Programming/CPP/Learn_Modern_CPP/Cpp_Code/02_Variables_Scopes_and_Namespaces/02-constants.cpp
@@ -0,0 +1,12 @@
+// 02-constants.cpp : introducing the const keyword
+ 
+#include <print>
+using namespace std;
+ 
+const double PI = 3.14159265358979;
+ 
+int main() {
+    auto const APPROX_E = 3;
+    println("pi is almost exactly {}, while e is approximately {}",
+        PI, APPROX_E);
+}
