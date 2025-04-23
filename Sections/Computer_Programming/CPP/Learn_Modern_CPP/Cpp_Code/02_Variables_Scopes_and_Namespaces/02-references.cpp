5966fe797173c5b1b6ab78cca3d5b0799dde9f64
Mark Khusid <markkhusid@protonmail.com>
2025-04-23 06:42:10 -0400

    added section references to CPP programming

---
+++ Sections/Computer_Programming/CPP/Learn_Modern_CPP/Cpp_Code/02_Variables_Scopes_and_Namespaces/02-references.cpp
@@ -0,0 +1,13 @@
+// 02-references.cpp : introducing l-value references
+ 
+#include <print>
+using namespace std;
+ 
+int alice_age{ 9 };
+ 
+int main() {
+    println("Alice\'s age is {}", alice_age);
+    int& alice_age_ref = alice_age;
+    alice_age_ref = 10;
+    println("Alice\'s age is now {}", alice_age);
+}
